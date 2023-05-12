7a722fb243ce49565b23db09fcddc3de8f748444
徐清凯 <id@xuqingkai.com>
2023-05-12 11:00:37 +0800

    Update rsa_private_decrypt.php

--- php/rsa_private_decrypt.php
+++ php/rsa_private_decrypt.php
@@ -1,3 +1,5 @@
 <?php
 $data='';
-openssl_private_decrypt($data ,$result ,$key);
+$rsa_private_key = '';
+$rsa_private = openssl_get_privatekey("-----BEGIN RSA PRIVATE KEY-----\n" . wordwrap($rsa_private_key, 64, "\n", true) . "\n-----END RSA PRIVATE KEY-----");
+openssl_private_decrypt($data ,$result ,$rsa_private);
