adc772f8817a098ee769af460a0f90b3402d1263
徐清凯 <id@xuqingkai.com>
2023-05-12 11:01:00 +0800

    Update rsa_private_sign.php

--- php/rsa_private_sign.php
+++ php/rsa_private_sign.php
@@ -3,7 +3,7 @@
 $data='';
 $rsa_private_key = '';
 
-$rsa_private = openssl_get_privatekey("-----BEGIN RSA PRIVATE KEY-----\n" . wordwrap($private_key, 64, "\n", true) . "\n-----END RSA PRIVATE KEY-----");
+$rsa_private = openssl_get_privatekey("-----BEGIN RSA PRIVATE KEY-----\n" . wordwrap($rsa_private_key, 64, "\n", true) . "\n-----END RSA PRIVATE KEY-----");
 openssl_sign($data, $rsa_byte_sign, $rsa_private, version_compare(PHP_VERSION,'5.4.8','>=') ? OPENSSL_ALGO_SHA256 : SHA256);
 
 $base64_sign = base64_encode($rsa_byte_sign);
