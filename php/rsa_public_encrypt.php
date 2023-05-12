1cf11ed1af0a8aa4e4fbf001717dfa26509216d8
徐清凯 <id@xuqingkai.com>
2023-05-12 11:01:20 +0800

    Update rsa_public_encrypt.php

--- php/rsa_public_encrypt.php
+++ php/rsa_public_encrypt.php
@@ -1,3 +1,5 @@
 <?php
 $data = '';
+$rsa_public_key = '';
+
 openssl_public_encrypt($data, $result, "-----BEGIN PUBLIC KEY-----\n" . wordwrap($rsa_public_key, 64, "\n", true) . "\n-----END PUBLIC KEY-----");
