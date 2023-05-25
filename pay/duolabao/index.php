7215950a2391bdc25d94ad72d74e02902e1d0436
徐清凯 <id@xuqingkai.com>
2023-05-25 18:00:08 +0800

    Update index.php

--- pay/duolabao/index.php
+++ pay/duolabao/index.php
@@ -1,1 +1,13 @@
-1
+<?php
+//用户编号
+$duolabao['customer_num'] = '';
+//店铺编号
+$duolabao['shop_num'] = '';
+//机具编号
+$duolabao['machine_num'] = '';
+//店铺名称
+$duolabao['shop_name'] = '';
+
+$duolabao['gateway'] = "https://openapi.duolabao.com";
+$duolabao['path'] = "/v1/customer/order/pay/create";
+?>
