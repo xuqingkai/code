df756fb58c8dad64a073ccccc0e29eb1b596841f
徐清凯 <id@xuqingkai.com>
2023-05-30 16:14:55 +0800

    Update uri.php

--- php/uri.php
+++ php/uri.php
@@ -19,4 +19,11 @@ $request_file = $_SERVER["SCRIPT_FILENAME"];
 $request_method = $_SERVER["REQUEST_METHOD"];
 
 //DOCUMENT_URI与PHP_SELF区别，前者实际请求什么就是什么，后者如果请求的是/结尾的地址，会附带index.php
+//如请求：/1.php/index/index?dd=2131
+//DOCUMENT_URI：/1.php/index/index
+//PHP_SELF：/1.php/index/index
+  
+//如请求：/1.php/index/index/?dd=2131
+//DOCUMENT_URI：/1.php/index/index
+//PHP_SELF：/1.php/index/index/index.php
 ?>
