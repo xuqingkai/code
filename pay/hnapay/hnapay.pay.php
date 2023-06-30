b5628122d148e54d25bfe05d17600b95f6b7cea9
徐清凯 <id@xuqingkai.com>
2023-06-30 11:27:30 +0800

    Update hnapay.pay.php

--- pay/hnapay/hnapay.pay.php
+++ pay/hnapay/hnapay.pay.php
@@ -1,27 +1,5 @@
 <?php
-//接口文档
-//https://www.yuque.com/chenyanfei-sjuaz/uhng8q/uoce7b
 
-//网关请求地址
-$hnapay['host']='https://gateway.hnapay.com';
-$hnapay['path']='/website/scanPay.do';
-
-//商户编号
-//https://portal.hnapay.com/
-$hnapay['merId']='';
-
-//报备编号
-//https://merchant.hnapay.com
-$hnapay['weChatMchId']='';
-
-//接口付款公钥
-$hnapay['public_key']='';
-$hnapay['public_key']=str_replace('-----BEGIN PUBLIC KEY-----','',str_replace('-----END PUBLIC KEY-----','',$hnapay['public_key']));
-$hnapay['public_key']=str_replace("\r",'',str_replace("\n",'',$hnapay['public_key']))
-//商户私钥
-$hnapay['private_key']='';
-$hnapay['private_key']=str_replace('-----BEGIN PRIVATE KEY-----','',str_replace('-----END PRIVATE KEY-----','',$hnapay['private_key']));
-$hnapay['private_key']=str_replace("\r",'',str_replace("\n",'',$hnapay['private_key']))
 
 $hnapay['data']=array();
 $hnapay['data']['version']='2.1';
