2b594f3576edb3282027639eb2de9ba60edd94b6
徐清凯 <id@xuqingkai.com>
2023-07-17 17:54:12 +0800

    Create CreateConsoleLoginUrl.php

---
+++ tencentcloud/dianziqian/CreateConsoleLoginUrl.php
@@ -0,0 +1,18 @@
+<?php
+CreateConsoleLoginUrl();
+function CreateConsoleLoginUrl(){
+    $header=array();
+    $header['X-TC-Action']='CreateConsoleLoginUrl';
+    $header['X-TC-Version']='2021-05-26';
+
+    $data=array();
+    $data['Agent']=array();
+    $data['Agent']['AppId']=config('appid');
+    $data['Agent']['ProxyOrganizationOpenId']='某公司ID';
+    $data['Agent']['ProxyOperator']=array();
+    $data['Agent']['ProxyOperator']['OpenId']='某人ID';
+    //$data['Agent']['ProxyAppId']='';
+    $data['ProxyOrganizationName']='某公司';
+    $json=json_encode($data);
+    return http_post($json, $header);
+}
