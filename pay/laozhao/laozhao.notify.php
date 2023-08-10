c2b73396a2207971f26dcbbb5a1a7a163a40f4fd
徐清凯 <id@xuqingkai.com>
2023-08-10 16:30:41 +0800

    Create laozhao.notify.php

---
+++ pay/laozhao/laozhao.notify.php
@@ -0,0 +1,9 @@
+<?php
+$laozhao['notify']=$_GET;
+if(intval($laozhao['notify']['opstate'])==0){
+  $laozhao['sign_string']='orderid='.$laozhao['notify']['orderid'].'&opstate='.$laozhao['notify']['opstate'].'&ovalue='.$laozhao['notify']['ovalue'].'';
+  $laozhao['sign']=md5($laozhao['sign_string'].$laozhao['mch_key']);
+  if($laozhao['notify']['sign']==$laozhao['sign']){
+    
+  }
+}
