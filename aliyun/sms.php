e1626391607e5b44a6a0e74c656c639405eb4107
徐清凯 <id@xuqingkai.com>
2023-08-11 09:05:52 +0800

    Update sms.php

--- aliyun/sms.php
+++ aliyun/sms.php
@@ -2,11 +2,13 @@
 include_once('./config.php');
 include_once('./function.php');
 function aliyun_sms($PhoneNumbers, $TemplateCode, $TemplateParam, $SignName){
-	$dict['Action']='SendSms';
-	$dict['Version']='2017-05-25';
-	$dict['RegionId']='cn-hangzhou';
-	$dict['PhoneNumbers']=$PhoneNumbers;
-	$dict['SignName']=$SignName;
-	$dict['TemplateCode']=$TemplateCode;
-	$dict['TemplateParam']=$TemplateParam;
+  $req['Action']='SendSms';
+  $req['Version']='2017-05-25';
+  $req['RegionId']='cn-hangzhou';
+  $req['PhoneNumbers']=$PhoneNumbers;
+  $req['SignName']=$SignName;
+  $req['TemplateCode']=$TemplateCode;
+  $req['TemplateParam']=$TemplateParam;
+  $res=aliyun_request($req);
+  return ;
 }
