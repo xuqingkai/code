504c36b3b21b6f7155ff503470943e44c7120daf
徐清凯 <xuqingkai@hotmail.com>
2024-05-29 11:15:26 +0800

    Update hnapay.pay.alipay.h5.php

--- pay/hnapay/hnapay.pay.alipay.h5.php
+++ pay/hnapay/hnapay.pay.alipay.h5.php
@@ -59,9 +59,7 @@ foreach($hnapay['data'] as $key=>$val){
 $html.='<input type="submit" value="submit" ><br />';
 $html.='</form>';
 $html.='<script>document.getElementsByTagName("form")[0].submit();</script>';
-$html='<!DOCTYPE html><html lang="zh">
-<head><meta http-equiv="Content-type" content="text/html; charset=utf-8" /><meta name="viewport" content="width=device-width, initial-scale=1.0" /></head>
-<body>'.$html.'</body></html>';
+$html='<!DOCTYPE html><html lang="zh"><head><meta http-equiv="Content-type" content="text/html; charset=utf-8" /><meta name="viewport" content="width=device-width, initial-scale=1.0" /></head><body>'.$html.'</body></html>';
 exit($html);
 
 
