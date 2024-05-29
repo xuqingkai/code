dc423755921aff4091df9831e9eaf2892f41a10e
徐清凯 <xuqingkai@hotmail.com>
2024-05-29 16:43:04 +0800

    Update hnapay.config.php

--- pay/hnapay/hnapay.config.php
+++ pay/hnapay/hnapay.config.php
@@ -9,15 +9,26 @@ $hnapay['host']='https://gateway.hnapay.com';
 //https://portal.hnapay.com/
 $hnapay['merId']='';
 
-//报备编号
+//微信报备编号
 //https://merchant.hnapay.com
 $hnapay['baobei_no']='';
+//微信接口付款公钥
+$hnapay['weixin_public_key']='';
+$hnapay['weixin_public_key']=str_replace('-----BEGIN PUBLIC KEY-----','',str_replace('-----END PUBLIC KEY-----','',$hnapay['weixin_public_key']));
+$hnapay['weixin_public_key']=str_replace("\r",'',str_replace("\n",'',$hnapay['weixin_public_key']));
+//微信商户私钥
+$hnapay['weixin_private_key']='';
+$hnapay['weixin_private_key']=str_replace('-----BEGIN RSA PRIVATE KEY-----','',str_replace('-----END RSA PRIVATE KEY-----','',$hnapay['weixin_private_key']));
+$hnapay['weixin_private_key']=str_replace("\r",'',str_replace("n",'',$hnapay['weixin_private_key']));
 
-//接口付款公钥
-$hnapay['public_key']='';
-$hnapay['public_key']=str_replace('-----BEGIN PUBLIC KEY-----','',str_replace('-----END PUBLIC KEY-----','',$hnapay['public_key']));
-$hnapay['public_key']=str_replace("\r",'',str_replace("\n",'',$hnapay['public_key']));
-//商户私钥
-$hnapay['private_key']='';
-$hnapay['private_key']=str_replace('-----BEGIN RSA PRIVATE KEY-----','',str_replace('-----END RSA PRIVATE KEY-----','',$hnapay['private_key']));
-$hnapay['private_key']=str_replace("\r",'',str_replace("n",'',$hnapay['private_key']));
+//支付宝报备编号
+//https://merchant.hnapay.com
+$hnapay['baobei_no']='';
+//支付宝接口付款公钥
+$hnapay['alipay_public_key']='';
+$hnapay['alipay_public_key']=str_replace('-----BEGIN PUBLIC KEY-----','',str_replace('-----END PUBLIC KEY-----','',$hnapay['alipay_public_key']));
+$hnapay['alipay_public_key']=str_replace("\r",'',str_replace("\n",'',$hnapay['alipay_public_key']));
+//支付宝商户私钥
+$hnapay['alipay_private_key']='';
+$hnapay['alipay_private_key']=str_replace('-----BEGIN RSA PRIVATE KEY-----','',str_replace('-----END RSA PRIVATE KEY-----','',$hnapay['alipay_private_key']));
+$hnapay['alipay_private_key']=str_replace("\r",'',str_replace("n",'',$hnapay['alipay_private_key']));
