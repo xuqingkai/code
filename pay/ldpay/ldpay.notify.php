f9a1766008edd8e7fe9a04414d0fae7bce3333c1
徐清凯 <xuqingkai@hotmail.com>
2024-05-31 14:32:44 +0800

    Update ldpay.notify.php

--- pay/ldpay/ldpay.notify.php
+++ pay/ldpay/ldpay.notify.php
@@ -9,6 +9,7 @@
 */
 include_once('./ldpay.config.php');
 $ldpay['data']=$_POST;
+if(!isset($ldpay['data']['mer_id'] )){exit('mer_id_null');}
 if($ldpay['data']['mer_id'] != $ldpay['mer_id']){ exit('mer_id_error'); }
 if($ldpay['data']['ret_code']!='0000' || $ldpay['data']['status']!='success'){ exit('fail'); }
 exit('success');
