a658509523b36ce3845e530a29f5bfe587f50268
徐清凯 <xuqingkai@hotmail.com>
2024-06-13 00:20:09 +0800

    Update rsa_public_encrypt.php

--- php/rsa_public_encrypt.php
+++ php/rsa_public_encrypt.php
@@ -3,3 +3,4 @@ $data = '';
 $rsa_public_key = '';
 
 openssl_public_encrypt($data, $result, "-----BEGIN PUBLIC KEY-----\n" . wordwrap($rsa_public_key, 64, "\n", true) . "\n-----END PUBLIC KEY-----");
+openssl_public_encrypt($data, $result, openssl_pkey_get_public("-----BEGIN CERTIFICATE-----\n".chunk_split(base64_encode(file_get_contents($cer_path)),64,"\n")."-----END CERTIFICATE-----\n"));
