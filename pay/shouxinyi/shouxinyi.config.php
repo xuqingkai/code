91f297a2be68c4fcc4596ec1ffbe37418d61a0a0
徐清凯 <xuqingkai@hotmail.com>
2024-06-13 11:48:25 +0800

    Create shouxinyi.config.php

---
+++ pay/shouxinyi/shouxinyi.config.php
@@ -0,0 +1,16 @@
+<?php
+
+//网关请求地址
+$shouxinyi['host']='https://apis.5upay.com';
+
+//商户编号
+$shouxinyi['merchantId']='';
+
+//商户私钥
+$shouxinyi['mch_pfx_file_path']='./client.pfx';
+
+//商户私钥密码
+$shouxinyi['mch_pfx_file_password']='';
+
+//接口公钥
+$shouxinyi['api_cer_file_path']='./server.cer';
