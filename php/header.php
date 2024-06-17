46d2d1c951a10fe202994434bee1858e11c726d5
徐清凯 <xuqingkai@hotmail.com>
2024-06-17 16:27:04 +0800

    Delete php/header.php

--- php/header.php
+++
@@ -1,7 +0,0 @@
-<?php
-$file=fopen('./header.txt',"a");
-foreach($_SERVER as $key=>$val){
-	fwrite($file, "【".$key."】\r\n".$val."\r\n-----------------------------------------------------------------------\r\n");
-}
-fclose($file);
-?>
