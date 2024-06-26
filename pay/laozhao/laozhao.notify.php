57c52f9151b11963d79c6b339040c3241808cdb0
kevin <aliyun@xuqingkai.com>
2024-06-26 10:15:37 +0800

    delete pay

--- pay/laozhao/laozhao.notify.php
+++
@@ -1,9 +0,0 @@
-<?php
-$laozhao['notify']=$_GET;
-if(intval($laozhao['notify']['opstate'])==0){
-  $laozhao['sign_string']='orderid='.$laozhao['notify']['orderid'].'&opstate='.$laozhao['notify']['opstate'].'&ovalue='.$laozhao['notify']['ovalue'].'';
-  $laozhao['sign']=md5($laozhao['sign_string'].$laozhao['mch_key']);
-  if($laozhao['notify']['sign']==$laozhao['sign']){
-    
-  }
-}
