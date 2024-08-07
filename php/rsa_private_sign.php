5d6d7a6220cda722e54e5b521921a2ddd28bb67a
徐清凯 <xuqingkai@hotmail.com>
2024-08-07 17:23:53 +0800

    Update rsa_private_sign.php

--- php/rsa_private_sign.php
+++ php/rsa_private_sign.php
@@ -1,8 +1,13 @@
 <?php
 //RSA私钥签名
 $data='';
-$rsa_private_key = '';
-
+$rsa_private_key = trim('');
+if(substr($rsa_private_key,0,5)!='----'){
+  if(substr($rsa_private_key,' RSA ')===false){
+    //转换pkcs
+  }
+  $rsa_private_key="-----BEGIN RSA PRIVATE KEY-----\r\n" . chunk_split($rsa_private_key, 64) . "-----END RSA PRIVATE KEY-----";
+}
 $rsa_private = openssl_get_privatekey("-----BEGIN RSA PRIVATE KEY-----\n" . wordwrap($rsa_private_key, 64, "\n", true) . "\n-----END RSA PRIVATE KEY-----");
 openssl_sign($data, $rsa_byte_sign, $rsa_private, version_compare(PHP_VERSION,'5.4.8','>=') ? OPENSSL_ALGO_SHA256 : SHA256);
 
