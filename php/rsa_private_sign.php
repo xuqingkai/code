343c60ec7e13739c6a5a44dc13e54f9f516995a3
徐清凯 <xuqingkai@hotmail.com>
2024-08-07 17:25:53 +0800

    Update rsa_private_sign.php

--- php/rsa_private_sign.php
+++ php/rsa_private_sign.php
@@ -8,7 +8,8 @@ if(substr($rsa_private_key,0,5)!='----'){
   }
   $rsa_private_key="-----BEGIN RSA PRIVATE KEY-----\r\n" . chunk_split($rsa_private_key, 64) . "-----END RSA PRIVATE KEY-----";
 }
-$rsa_private = openssl_get_privatekey("-----BEGIN RSA PRIVATE KEY-----\n" . wordwrap($rsa_private_key, 64, "\n", true) . "\n-----END RSA PRIVATE KEY-----");
+//openssl_get_privatekey函数7.2.0已被废弃，建议用openssl_pkey_get_private
+$rsa_private = openssl_pkey_get_private("-----BEGIN RSA PRIVATE KEY-----\n" . wordwrap($rsa_private_key, 64, "\n", true) . "\n-----END RSA PRIVATE KEY-----");
 openssl_sign($data, $rsa_byte_sign, $rsa_private, version_compare(PHP_VERSION,'5.4.8','>=') ? OPENSSL_ALGO_SHA256 : SHA256);
 
 $base64_sign = base64_encode($rsa_byte_sign);
