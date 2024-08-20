f7cbce7d11a7497e766638790b664bc8f560f36a
徐清凯 <xuqingkai@hotmail.com>
2024-08-20 08:49:14 +0800

    Update curl.php

--- php/curl.php
+++ php/curl.php
@@ -2,9 +2,9 @@
 function http_curl($url,$data,$header){
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($curl, CURLOPT_HEADER, false);//是否返回headers信息
+    curl_setopt($curl, CURLOPT_HEADER, true);//是否返回headers信息
     curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-type: application/x-www-form-urlencoded", 'Content-Length: '.strlen($data)));
-    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POST, strlen($data)>0);
     //curl_setopt($curl, CURLOPT_ENCODING,'gzip');
     curl_setopt($curl, CURLOPT_POSTFIELDS , $data);
     curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);//忽略重定向
