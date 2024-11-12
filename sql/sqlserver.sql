d14536eb75b38af7bd4a6ccc885b20b3a4113968
徐清凯 <xuqingkai@hotmail.com>
2024-11-12 12:28:16 +0800

    Update and rename sqlserver.md to sqlserver.sql

--- sql/sqlserver.sql
+++ sql/sqlserver.sql
@@ -1,4 +1,4 @@
-```
+
 -- 表
 IF EXISTS (SELECT * FROM sys.objects WHERE name = 'xqk_user') DROP TABLE xqk_user;
 CREATE TABLE [xqk_user](
@@ -31,9 +31,9 @@ INSERT INTO [xqk_user] ([user_id], [user_name], [user_salt], [password], [nick_n
 INSERT [xqk_user] ([user_id], [user_name], [user_salt], [password], [nick_name], [sex], [age], [balance], [create_datetime], [contents]) VALUES ('64648485.b33aed8f3134996703dc39f9a7c95783', 'agent', '93bb7044b3513e30e29b2cbf973ea32c', '186c42b43f7ac5cba3bfa6ae7653803b', '代理商', 1, 44, 4875.23, '2023-05-17 15:38:45', '代理商下游有很多用户');
 INSERT [xqk_user] ([user_id], [user_name], [user_salt], [password], [nick_name], [sex], [age], [balance], [create_datetime], [contents]) VALUES ('64648486.ee11cbb19052e40b07aac0ca060c23ee', 'user', '9b2eea4b45bcdd5c3b342456bdff139d', '8ed11b05caf627ebefabf2aa6409d8bb', '用户', 0, 33, 357.44, '2023-05-17 15:38:46', '普通用户是最小的服务单元');
 INSERT [xqk_user] ([user_id], [user_name], [user_salt], [password], [nick_name], [sex], [age], [balance], [create_datetime], [contents]) VALUES ('64648487.098f6bcd4621d373cade4e832627b4f6', 'test', '8607f985d1a84dc6c670673040dd9a56', 'd4aeff0f0f41e37b61d6f704d8ae3e32', '测试', 0, 22, 29.68, '2023-05-17 15:38:47', '测试账号');
-```
 
-```
+
+
 IF EXISTS (SELECT * FROM sys.objects WHERE name = 'xqk_ip_log') DROP TABLE xqk_ip_log;
 CREATE TABLE [xqk_ip_log](
 	[id] [int] IDENTITY(1,1) NOT NULL,
@@ -47,4 +47,4 @@ CREATE TABLE [xqk_ip_log](
 
 INSERT INTO [xqk_ip_log] ([ip], [address], [create_date], [create_datetime], [create_timestamp]) VALUES ('127.0.0.1', '本地', '1970-01-01', '1970-01-01 08:00:01', 1);
 
-```
+
