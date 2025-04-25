15c71defbdd55ea116ae1bd396c01310a8920876
徐清凯 <4472979+xuqingkai@users.noreply.github.com>
2025-04-25 19:42:31 +0800

    Update and rename file_get_contents_post.php to http_file.php

---
+++ php/http_file.php
@@ -0,0 +1,24 @@
+<?php
+function http_file($url, $body='', $header=array()){
+    global $xuqingkai;
+    $headers=array_merge($headers, array('Author'=>'xuqingkai'));
+    $request_headers=array(); foreach($headers as $key=>$val){ $request_headers[]=$key.':'.$val; }
+
+    $error=false;
+    $response_header=array();
+    try{
+        $response_body=file_get_contents($url, false, stream_context_create(array(
+            'http'=>array(
+                'method'=>strlen($body)>0?'POST':'GET',
+                'header'=>implode("\r\n",$request_headers),
+                'content'=>$body
+            ),
+            'ssl'=>array('verify_peer'=>false,'verify_peer_name'=>false)
+        )));
+        $response_header=$http_response_header;
+    }catch(Exception $ex){
+        $error=$ex->getMessage();
+    }
+    return [$error, $response_header, $response_body];
+}
+//list($error, $header, $response)=http_curl();
