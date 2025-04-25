b544e2d57219b3387e4877dc196b9dfb08bf35d5
徐清凯 <4472979+xuqingkai@users.noreply.github.com>
2025-04-25 23:02:53 +0800

    Update http_file.php

--- php/http_file.php
+++ php/http_file.php
@@ -1,6 +1,5 @@
 <?php
 function http_file($url, $body='', $headers=array()){
-    global $xuqingkai;
     if(!$headers){
         $headers=array(
             'User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 Edg/135.0.0.0',
